2ad67f3ab64c30eabe9b8a98ef672d3c1e8dd0ba
quadseed <contact@quadseed.net>
2025-10-19 12:14:23 +0900

    Revert episode.tsx to d0dc28c2486c989c8958744df4a84c0a64db357c

--- app/routes/episode.tsx
+++ app/routes/episode.tsx
@@ -165,7 +165,7 @@ export default function Episode({ loaderData }: { loaderData: LoaderData }) {
   useEffect(() => {
     // location.state から autoPlay を取得
     const state = window.history.state && window.history.state.usr
-    setAutoPlay(state && state.autoPlay === false ? false : true)
+    setAutoPlay(state && state.autoPlay === true)
   }, [episodeData.episode_id])
 
   return (
